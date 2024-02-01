0506e8f8930512c60fd31e9d5fdf568b05f250c9
幹人 永澤 <230413@jc-21.jp>
2024-02-01 11:35:19 +0900

    あ

--- Player.cpp
+++ Player.cpp
@@ -1,8 +1,10 @@
 #include "Player.h"
 #include"Engine/Model.h"
 #include"Engine/Input.h"
+#include"Engine/Debug.h"
+
 namespace {
-	const float PLAYER_MOVE_SPEED{ 0.1f };
+	const float PLAYER_MOVE_SPEED{ 1.0f };
 }
 Player::Player(GameObject* parent)
 	:GameObject(parent,"Player"),hModel_(-1),speed_(PLAYER_MOVE_SPEED)
@@ -15,7 +17,8 @@ void Player::Initialize()
 	assert(hModel_ >= 0);
 	transform_.position_.x = 0.5;
 	transform_.position_.z = 1.5;
-
+	//map[13][1]が、（map[y][x]の場合）初期位置
+	
 }
 
 void Player::Update()
@@ -27,51 +30,58 @@ void Player::Update()
 	//};
 	//int moveDir = Dir::NONE;
 	XMVECTOR vFront = { 0,0,1,0 };
-	XMVECTOR move{ 0,0,1,0 };//移動初期化
+	XMVECTOR move{ 0,0,0,0 };//移動初期化
 
-	if (Input::IsKey(DIK_LEFT))
+	if (Input::IsKeyDown(DIK_LEFT))
 	{
 		//transform_.rotate_.y = -90;
 		//transform_.position_.x -= 0.1f;
-		
+		 
 		//moveDir = Dir::LEFT;
 		move = XMVECTOR{ -1,0,0,0 };
 	}
-	if (Input::IsKey(DIK_RIGHT))
+	if (Input::IsKeyDown(DIK_RIGHT))
 	{
 		//transform_.rotate_.y = 90;
 		//transform_.position_.x += 0.1f;
 		move = XMVECTOR{ 1,0,0,0 };
 		//moveDir = Dir::RIGHT;
 	}
-	if (Input::IsKey(DIK_UP))
+	if (Input::IsKeyDown(DIK_UP))
 	{
 		//transform_.rotate_.y = 0;
 		//transform_.position_.z += 0.1f;
 		move = XMVECTOR{ 0,0,1,0 };
 		//moveDir = Dir::UP;
 	}
-	if (Input::IsKey(DIK_DOWN))
+	if (Input::IsKeyDown(DIK_DOWN))
 	{
 		//transform_.rotate_.y = -180;
 		//transform_.position_.z -= 0.1f;
 		move = XMVECTOR{ 0,0,-1,0 };
 		//moveDir = Dir::DOWN;
 	}
-	//キャラの移動で必要↓
+	//キャラの移動で必要↓//キャラの向きcosθで求める
 	XMVECTOR pos = XMLoadFloat3(&(transform_.position_));
 	pos = pos + speed_ * move;
-	XMStoreFloat3(&(transform_.position_), pos);
+	Debug::Log("(X,Z)=");
+	Debug::Log(XMVectorGetX(pos));
+	Debug::Log(",");
+	//Debug::Log()
+	if (!XMVector3Equal(move, XMVectorZero())) {
+		XMStoreFloat3(&(transform_.position_), pos);
 
-	XMVECTOR vdot= XMVector3Dot(vFront, move);
-	float angle = acos(XMVectorGetX(vdot));
+		XMVECTOR vdot = XMVector3Dot(vFront, move);
+		assert(XMVectorGetX(vdot) <= 1 && XMVectorGetX(vdot)  >=-1);
+		float angle = acos(XMVectorGetX(vdot));
 
-	XMVECTOR vCross = XMVector3Cross(vFront, move);
-	if (XMVectorGetY(vCross) < 0)
-	{
-		angle *= -1;
+		XMVECTOR vCross = XMVector3Cross(vFront, move);
+		if (XMVectorGetY(vCross) < 0)
+		{
+			angle *= -1;
+		}
+		transform_.rotate_.y = XMConvertToDegrees(angle);
 	}
-	transform_.rotate_.y = XMConvertToDegrees(angle);
 	//キャラの向き
 	//float rotAngle[5]{ -90,90,0,180,180 };
 	//transform_.rotate_.y = rotAngle[moveDir];
